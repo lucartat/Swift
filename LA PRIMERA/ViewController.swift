dea7e993faae5fa4c0d9173c519ba9e7305abdcb
TEMPORAL LUIS PROGRAMADOR <temporalluisprogramador@carloss-macbook-pro.local>
2017-09-22 09:29:19 -0500

    Initial Commit

---
+++ LA PRIMERA/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LA PRIMERA
+//
+//  Created by TEMPORAL LUIS PROGRAMADOR on 9/22/17.
+//  Copyright © 2017 ORGANIZACION LUIS ENIRIQUE PARADA SOLORZANO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
